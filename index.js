451a34c4c38f4b5122d4d0ea7b0a8048e474a698
Andrea Alexandra Monroy Carrillo <andrea.monroy@globant.com>
2022-03-28 12:05:45 -0500

    add cors

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const cors = require('cors');
 const express = require('express');
 const app = express();
-app.use(cors());
+app.use(cors({origin:true,credentials: true}));
 const { config } = require('./config/index');
 const loginApi = require('./routes/login.js');
 
